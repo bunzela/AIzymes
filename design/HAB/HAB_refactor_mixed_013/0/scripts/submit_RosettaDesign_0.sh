69d2c0017a14cf6a10f0feeafc479d8b3c48d750
Adrian Bunzel <adrian.bunzel@googlemail.com>
2024-10-08 10:58:00 +0200

    updated AIZYMES 13

---
+++ design/HAB/HAB_refactor_mixed_013/0/scripts/submit_RosettaDesign_0.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#$ -V
+#$ -cwd
+#$ -N MIX_RosettaDesign_0
+#$ -hard -l mf=10G
+#$ -o /home/bunzelh/AIzymes/design/HAB/HAB_refactor_mixed_013/0/scripts/RosettaDesign_0.out
+#$ -e /home/bunzelh/AIzymes/design/HAB/HAB_refactor_mixed_013/0/scripts/RosettaDesign_0.err
+
+# Output folder
+cd /home/bunzelh/AIzymes/design/HAB/HAB_refactor_mixed_013/0
+pwd
+bash /home/bunzelh/AIzymes/design/HAB/HAB_refactor_mixed_013/0/scripts/RosettaDesign_0.sh
---
+++ design/HAB/HAB_refactor_mixed_013/2/scripts/submit_RosettaDesign_2.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#$ -V
+#$ -cwd
+#$ -N MIX_RosettaDesign_2
+#$ -hard -l mf=10G
+#$ -o /home/bunzelh/AIzymes/design/HAB/HAB_refactor_mixed_013/2/scripts/RosettaDesign_2.out
+#$ -e /home/bunzelh/AIzymes/design/HAB/HAB_refactor_mixed_013/2/scripts/RosettaDesign_2.err
+
+# Output folder
+cd /home/bunzelh/AIzymes/design/HAB/HAB_refactor_mixed_013/2
+pwd
+bash /home/bunzelh/AIzymes/design/HAB/HAB_refactor_mixed_013/2/scripts/RosettaDesign_2.sh
