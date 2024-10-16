d8de0755d12149e78c44cfe4de7542a6dd3be76a
Abbie Lear <abbie@barcombe.net>
2024-10-16 16:52:40 +0100

    New design module creates correct cmd for shell script

--- src/design_MDMin_001.py
+++ src/design_MDMin_001.py
@@ -59,6 +59,14 @@ trajin    {PDBfile_out}_new_bb.pdb
 rmsd      @CA ref [ref]
 trajout   {PDBfile_out}_aligned.pdb noter
 '''
+    
+#     cpptraj = f'''parm    {PDBfile_ligand}.pdb [ref_parm]
+# parm    {PDBfile}.pdb [struc_parm]
+# reference {PDBfile_ligand}.pdb parm [ref_parm] [ref]
+# trajin    {PDBfile}.pdb parm [struc_parm]
+# rmsd      @CA ref [ref]
+# trajout   {PDBfile_out}_aligned.pdb parm [struc_parm] noter
+# '''
     with open(f'{PDBfile_out}_CPPTraj_aligned.in','w') as f: f.write(cpptraj) 
  
     cmd += f"""
@@ -85,12 +93,43 @@ cat {PDBfile_out}_lig.pdb     >> {PDBfile_out}_input.pdb
 sed -i '/TER/d' {PDBfile_out}_input.pdb
     """
 
+#Write Rosetta input to build in sidechains
+    Rosetta_xml = f"""
+<ROSETTASCRIPTS>
+    <MOVERS>
+        <DumpPdb name="output_pdb" fname="{PDBfile_out}_wRosetta_sidechains.pdb"/>
+    </MOVERS>
+    <PROTOCOLS>
+        <Add mover_name="output_pdb" />
+    </PROTOCOLS>
+</ROSETTASCRIPTS>
+"""
+    with open(f'{filename}/scripts/Rosetta_buildPDB_{index}.xml', 'w') as f:
+        f.write(Rosetta_xml)
+
+#Add Rosetta command to shell script
+    cmd += f"""
+# Run Rosetta Relax
+{self.ROSETTA_PATH}/bin/rosetta_scripts.{self.rosetta_ext} \
+                -s                        {PDBfile_out}_input.pdb \
+                -extra_res_fa             {self.FOLDER_INPUT}/{self.LIGAND}.params \
+                -parser:protocol          {filename}/scripts/Rosetta_buildPDB_{index}.xml \
+                -nstruct                  1 \
+                -ignore_zero_occupancy    false \
+                -corrections::beta_nov16  true \
+                -run:preserve_header      true \
+                    
+# Tidy up the output file
+sed -i -e '/[0-9]H/d' -e '/H[0-9]/d' -e '/CONECT/Q' {PDBfile_out}_wRosetta_sidechains.pdb
+"""
+
     with open(f"{PDBfile_out}_tleap.in", "w") as f:
         f.write(f"""source leaprc.protein.ff19SB 
 source leaprc.gaff
 loadamberprep   {self.FOLDER_INPUT}/5TS.prepi
 loadamberparams {self.FOLDER_INPUT}/5TS.frcmod
-mol = loadpdb {PDBfile_out}_input.pdb
+mol = loadpdb {PDBfile_out}_wRosetta_sidechains.pdb
+set default pbradii mbondi3
 saveamberparm mol {PDBfile_out}.parm7 {PDBfile_out}.rst7
 quit
 """)
@@ -102,28 +141,28 @@ tleap -s -f {PDBfile_out}_tleap.in &> {PDBfile_out}_tleap.out
     # Create the input file    
     in_file = f"""
 initial minimization
-&cntrl 
-    imin           = 1, 
-    ntmin          = 0,
-    ncyc           = 500,
-    maxcyc         = 1000, 
-    ntpr           = 100, 
-    ntb            = 1,         !constant volume  
-    nmropt         = 1,         !Restraints
+&cntrl                                                                         
+    imin           = 1,                                                        
+    ntmin          = 0,                                                        
+    ncyc           = 500,                                                      
+    maxcyc         = 1000,                                                      
+    ntpr           = 100,                                                                                                                   
+    ntb=0                                                                      
+    igb = 8                                                                    
+    cut = 1000,                                                                                                                   
 &end 
-&wt TYPE='END'/ 
 """    
     # Write the Amber.in to a file
     with open(f'{filename}/scripts/MDmin_{index}.in', 'w') as f:
         f.writelines(in_file)  
 
     cmd += f"""
-# Run Rosetta Relax
-sander -O -i {filename}/scripts/MDmin_{index}.in' \
+# Run MD
+sander -O -i {filename}/scripts/MDmin_{index}.in \
           -c {PDBfile_out}.rst7 -p {PDBfile_out}.parm7 \
           -o {PDBfile_out}_MD.log -x {PDBfile_out}_MD.nc -r {PDBfile_out}_MD_out.rst7 -inf {PDBfile_out}_MD_out.mdinf
 
-ambpdb -p {PDBfile_out}.parm7 < {PDBfile_out}_MD_out.rst7 > {self.WT}_MDMin_{index}.pdb
+cpptraj -p {PDBfile_out}.parm7 -y {PDBfile_out}_MD_out.rst7 -x {self.WT}_MDMin_{index}.pdb
 
 # Clean the output file
 sed -i '/        H  /d' {self.WT}_MDMin_{index}.pdb
