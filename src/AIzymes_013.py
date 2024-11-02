c55562f409ab87e4f23a7b8b551485e8f5397a06
Bunzel  Adrian <bunzelh@ethz.ch>
2024-11-02 10:51:14 +0100

    documentation

--- src/AIzymes_013.py
+++ src/AIzymes_013.py
@@ -19,89 +19,132 @@ from plotting_001             import *
 #from setup_system_001         import *
 # -------------------------------------------------------------------------------------------------------------------------
 
-class AIzymes_MAIN():
+class AIzymes_MAIN:
+    """
+    Main class for managing AIzymes workflow, including setup, initialization, control, and plotting functions.
+    """
 
     def __init__(self):
-        
+        """
+        Initializes an instance of the AIzymes_MAIN class.
+        """
         return
-         
+
     def setup(self, FOLDER_HOME, FOLDER_PARENT, CST_NAME, WT, LIGAND, DESIGN,
-      MAX_JOBS          = 100,
-      N_PARENT_JOBS     = 3,
-      MAX_DESIGNS       = 10000,
-      KBT_BOLTZMANN     = [0.5, 0.0003],
-      CST_WEIGHT        = 1.0,
-      ProteinMPNN_PROB  = 0.0,
-      ProteinMPNN_BIAS  = 0.0,
-      LMPNN_PROB        = 0.0,
-      FOLDER_MATCH      = None,
-      ProteinMPNN_T     = "0.1",
-      LMPNN_T           = "0.1",
-      LMPNN_BIAS        = 0.0,
-      SUBMIT_PREFIX     = None,
-      SYSTEM            = None,
-      MATCH             = None,  
-      ROSETTA_PATH      = "/home/bunzelh/rosetta_src_2021.16.61629_bundle/main/source/",
-      EXPLORE           = False,
-      FIELD_TOOLS       = 'src/FieldTools.py',
-      LOG               = 'debug',
-      PARENT_DES_MED    = 'RosettaDesign'
-      ):
-                   
-        # Automatically assign all parameters to instance variables
+              MAX_JOBS=100, N_PARENT_JOBS=3, MAX_DESIGNS=10000, KBT_BOLTZMANN=[0.5, 0.0003],
+              CST_WEIGHT=1.0, ProteinMPNN_PROB=0.0, ProteinMPNN_BIAS=0.0, LMPNN_PROB=0.0, 
+              FOLDER_MATCH=None, ProteinMPNN_T="0.1", LMPNN_T="0.1", LMPNN_BIAS=0.0, 
+              SUBMIT_PREFIX=None, SYSTEM=None, MATCH=None, ROSETTA_PATH="/home/bunzelh/rosetta_src_2021.16.61629_bundle/main/source/",
+              EXPLORE=False, FIELD_TOOLS='src/FieldTools.py', LOG='debug', PARENT_DES_MED='RosettaDesign'):
+        """
+        Sets up the AIzymes project environment with specified parameters.
+
+        Parameters:
+            FOLDER_HOME (str): Path to the main folder.
+            FOLDER_PARENT (str): Path to the parent folder.
+            CST_NAME (str): Constraint name.
+            WT (str): Wild type information.
+            LIGAND (str): Ligand data.
+            DESIGN (str): Design specifications.
+            MAX_JOBS (int): Maximum number of jobs to run concurrently.
+            N_PARENT_JOBS (int): Number of parent jobs.
+            MAX_DESIGNS (int): Maximum number of designs.
+            KBT_BOLTZMANN (list): Boltzmann constant values.
+            CST_WEIGHT (float): Constraint weight.
+            ProteinMPNN_PROB (float): Probability parameter for ProteinMPNN.
+            ProteinMPNN_BIAS (float): Bias parameter for ProteinMPNN.
+            LMPNN_PROB (float): Probability parameter for LMPNN.
+            FOLDER_MATCH (str): Path to match folder.
+            ProteinMPNN_T (str): Temperature for ProteinMPNN.
+            LMPNN_T (str): Temperature for LMPNN.
+            LMPNN_BIAS (float): Bias parameter for LMPNN.
+            SUBMIT_PREFIX (str): Submission prefix.
+            SYSTEM (str): System information.
+            MATCH (str): Match specifications.
+            ROSETTA_PATH (str): Path to Rosetta source.
+            EXPLORE (bool): Whether to explore parameter space.
+            FIELD_TOOLS (str): Path to FieldTools script.
+            LOG (str): Logging level.
+            PARENT_DES_MED (str): Parent design method.
+        """
         for key, value in locals().items():
             if key not in ['self']:  
                 setattr(self, key, value)
         
-        # Run aizymes_setup
         aizymes_setup(self)
         
-        print('''AIzymes initiated.
-''')
-        
-  
-    def initialize(self, FOLDER_HOME,
-                   UNBLOCK_ALL     = False, 
-                   PRINT_VAR       = True,
-                   PLOT_DATA       = False,
-                   LOG             = 'debug'):
-        
-        # Automatically assign all parameters to instance variables
+        print("AIzymes initiated.")
+
+    def initialize(self, FOLDER_HOME, UNBLOCK_ALL=False, PRINT_VAR=True, PLOT_DATA=False, LOG='debug'):
+        """
+        Initializes AIzymes with given parameters.
+
+        Parameters:
+            FOLDER_HOME (str): Path to the main folder.
+            UNBLOCK_ALL (bool): Flag to unblock all processes.
+            PRINT_VAR (bool): Flag to print variables.
+            PLOT_DATA (bool): Flag to plot data.
+            LOG (str): Logging level.
+        """
         for key, value in locals().items():
             if key not in ['self']:  
                 setattr(self, key, value)
                               
         initialize_controller(self, FOLDER_HOME)
-        
-    def controller(self,
-                   HIGHSCORE       = 0.70,
-                   NORM            = {'interface_score': [10, 35],
-                                      'total_score': [200, 500], 
-                                      'catalytic_score': [-40, 0], 
-                                      'efield_score': [10, 220]}):
 
-        # Automatically assign all parameters to instance variables
+    def controller(self, HIGHSCORE=0.70, NORM=None):
+        """
+        Controls the AIzymes project based on scoring and normalization parameters.
+
+        Parameters:
+            HIGHSCORE (float): High score threshold for evaluation.
+            NORM (dict): Normalization values for different scores.
+        """
+        if NORM is None:
+            NORM = {
+                'interface_score': [10, 35],
+                'total_score': [200, 500], 
+                'catalytic_score': [-40, 0], 
+                'efield_score': [10, 220]
+            }
+
         for key, value in locals().items():
             if key not in ['self']:
                 setattr(self, key, value)
                 
         start_controller(self)
         
-    def plot(self, main_plots=True, tree_plot=True, landscape_plot=True, print_vals=True,
-        NORM = {'interface_score': [10, 35],
+    def plot(self, main_plots=True, tree_plot=True, landscape_plot=True, print_vals=True, NORM=None, HIGHSCORE_NEGBEST=None):
+        """
+        Generates plots based on AIzymes data, including main, tree, and landscape plots.
+
+        Parameters:
+            main_plots (bool): Flag to generate main plots.
+            tree_plot (bool): Flag to generate tree plot.
+            landscape_plot (bool): Flag to generate landscape plot.
+            print_vals (bool): Flag to print values on plots.
+            NORM (dict): Normalization values for different scores.
+            HIGHSCORE_NEGBEST (dict): High score and negative best score for different metrics.
+        """
+        if NORM is None:
+            NORM = {
+                'interface_score': [10, 35],
                 'total_score': [200, 500], 
                 'catalytic_score': [-40, 0], 
-                'efield_score': [10, 220]},
-        HIGHSCORE_NEGBEST = {'HIGHSCORE_combined_score': 0.814,
-                             'NEGBEST_combined_score': 0.503,
-                             'HIGHSCORE_total_score': 0.954,
-                             'NEGBEST_total_score': 0.209 ,
-                             'HIGHSCORE_interface_score': 0.994,
-                             'NEGBEST_interface_score': 0.935 ,
-                             'HIGHSCORE_efield_score': 0.970,
-                             'NEGBEST_efield_score': 0.807}):
+                'efield_score': [10, 220]
+            }
+        if HIGHSCORE_NEGBEST is None:
+            HIGHSCORE_NEGBEST = {
+                'HIGHSCORE_combined_score': 0.814,
+                'NEGBEST_combined_score': 0.503,
+                'HIGHSCORE_total_score': 0.954,
+                'NEGBEST_total_score': 0.209,
+                'HIGHSCORE_interface_score': 0.994,
+                'NEGBEST_interface_score': 0.935,
+                'HIGHSCORE_efield_score': 0.970,
+                'NEGBEST_efield_score': 0.807
+            }
 
-        # Automatically assign all parameters to instance variables
         for key, value in locals().items():
             if key not in ['self']:
                 setattr(self, key, value)
