ee09abcc781195fbe8654c028661b39c8c05cfca
Bunzel  Adrian <bunzelh@ethz.ch>
2024-11-04 07:56:33 +0100

    testing design

--- design/HAB/HAB_refactor_standard_013/7/scripts/ESMfold_7.sh
+++
@@ -1,5 +0,0 @@
-
-    
- python /home/bunzelh/AIzymes/design/HAB/HAB_refactor_standard_013/parent/ESMfold.py --sequence_file /home/bunzelh/AIzymes/design/HAB/HAB_refactor_standard_013/7/7vuu_7.seq --output_file   /home/bunzelh/AIzymes/design/HAB/HAB_refactor_standard_013/7/7vuu_ESMfold_7.pdb 
-
-sed -i '/PARENT N\/A/d' /home/bunzelh/AIzymes/design/HAB/HAB_refactor_standard_013/7/7vuu_ESMfold_7.pdb
