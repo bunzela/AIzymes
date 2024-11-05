1b3677c2c421918a42dffa41c3a9de25f020a1ca
Bunzel  Adrian <bunzelh@ethz.ch>
2024-11-05 15:37:56 +0100

    deleted old files

--- design/HAB/HAB_refactor_mixed_013/3/scripts/ESMfold_3.sh
+++
@@ -1,5 +0,0 @@
-
-    
- python /home/bunzelh/AIzymes/design/HAB/HAB_refactor_mixed_013/parent/ESMfold.py --sequence_file /home/bunzelh/AIzymes/design/HAB/HAB_refactor_mixed_013/3/7vuu_3.seq --output_file   /home/bunzelh/AIzymes/design/HAB/HAB_refactor_mixed_013/3/7vuu_ESMfold_3.pdb 
-
-sed -i '/PARENT N\/A/d' /home/bunzelh/AIzymes/design/HAB/HAB_refactor_mixed_013/3/7vuu_ESMfold_3.pdb
