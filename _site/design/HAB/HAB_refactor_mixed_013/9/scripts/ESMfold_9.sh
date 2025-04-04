2821df1a7f1b5cccdb339cc6b8cfd51c8cace0fe
Bunzel  Adrian <bunzelh@ethz.ch>
2025-04-04 17:03:33 +0200

    clean up. unnnecsseary stuff deleted or moved to AIzymes_private
    .

--- _site/design/HAB/HAB_refactor_mixed_013/9/scripts/ESMfold_9.sh
+++
@@ -1,5 +0,0 @@
-
-    
- python /home/bunzelh/AIzymes/design/HAB/HAB_refactor_mixed_013/parent/ESMfold.py --sequence_file /home/bunzelh/AIzymes/design/HAB/HAB_refactor_mixed_013/9/7vuu_9.seq --output_file   /home/bunzelh/AIzymes/design/HAB/HAB_refactor_mixed_013/9/7vuu_ESMfold_9.pdb 
-
-sed -i '/PARENT N\/A/d' /home/bunzelh/AIzymes/design/HAB/HAB_refactor_mixed_013/9/7vuu_ESMfold_9.pdb
